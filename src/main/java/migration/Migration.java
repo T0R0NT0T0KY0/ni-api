1d7d818b3df8953401dcb2ba8d835c7dff378974
Dmitry <79139018+T0R0NT0T0KY0@users.noreply.github.com>
2021-10-02 13:15:16 +0300

    Update Migration.java

--- src/main/java/migration/Migration.java
+++ src/main/java/migration/Migration.java
@@ -5,8 +5,8 @@ import org.flywaydb.core.Flyway;
 public class Migration {
 	public static void main(String[] args) {
 		Flyway flyway = new Flyway();
-		flyway.setDataSource("jdbc:postgresql://localhost:5432/new_vk_api?currentSchema=public",
-				"postgres", "3242091046");
+		flyway.setDataSource("db_url?currentSchema=public",
+				"user", "pasword");
 		flyway.repair();
 		flyway.migrate();
 	}
